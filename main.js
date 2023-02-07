194bfbabe30e087facf68a2aee64ea87dfb09db9
Aranog-blip <123972330+Aranog-blip@users.noreplay.github.com>
2023-02-07 22:55:27 +0100

    Made working prompt for user

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ let compGuess = ["ROCK", "PAPER", "SCISSORS"];
 let randomChoice = Math.floor(Math.random()*compGuess.length);
 
 console.log (compGuess[randomChoice]);
+
+let userChoice = prompt("Choose one: ROCK, PAPER OR SCISSORS!!!");
+console.log(userChoice)
