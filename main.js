655f60bf5df8f81d4aae7928ba181a0da9276e99
Aranog-blip <123972330+Aranog-blip@users.noreplay.github.com>
2023-02-08 00:35:04 +0100

    Made all the results work, now the project is complete

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ let randomChoice = Math.floor(Math.random()*compGuess.length);
 console.log (compGuess[randomChoice]);
 
 
-
-
-if ((userChoice) == (compGuess[randomChoice])){
+if ((userChoice) == (compGuess[randomChoice])) {
     alert("IT'S A DRAW!");
-}else if ((((userChoice = "ROCK") && (compGuess[randomChoice] = "SCISSORS")) || ((userChoice = "PAPER") && (compGuess[randomChoice] = "ROCK"))) || ((userChoice = "SCISSORS") && (compGuess[randomChoice] = "PAPER"))){
+
+}else if (userChoice == compGuess[0] && compGuess[randomChoice] == compGuess[2]){
     alert("YOU WON!");
+
+}else if (userChoice == compGuess[1] && compGuess[randomChoice] == compGuess[0]){
+    alert("YOU WON!");
+
+}else if (userChoice == compGuess[2] && compGuess[randomChoice] == compGuess[1]){
+    alert("YOU WON!");
+
 } else {
     alert("YOU LOST!");
 }
