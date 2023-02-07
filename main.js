f8593c1206cd3c93da463be5557b2f83192bfeb6
Aranog-blip <123972330+Aranog-blip@users.noreplay.github.com>
2023-02-07 23:22:11 +0100

    Finished but the conditinals dont work and the result is always you won

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let compGuess = ["ROCK", "PAPER", "SCISSORS"];
-
-let randomChoice = Math.floor(Math.random()*compGuess.length);
-
-console.log (compGuess[randomChoice]);
-
 let userChoice = prompt("Choose one: ROCK, PAPER OR SCISSORS!!!");
 console.log(userChoice)
+
+let compGuess = ["ROCK", "PAPER", "SCISSORS"];
+let randomChoice = Math.floor(Math.random()*compGuess.length);
+console.log (compGuess[randomChoice]);
+
+
+
+
+if ((userChoice) == (compGuess[randomChoice])){
+    alert("IT'S A DRAW!");
+}else if ((((userChoice = "ROCK") && (compGuess[randomChoice] = "SCISSORS")) || ((userChoice = "PAPER") && (compGuess[randomChoice] = "ROCK"))) || ((userChoice = "SCISSORS") && (compGuess[randomChoice] = "PAPER"))){
+    alert("YOU WON!");
+} else {
+    alert("YOU LOST!");
+}
+
