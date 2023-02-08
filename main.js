b8f290c710efd920644329c19b2e4a90efcf5da5
Aranog-blip <123972330+Aranog-blip@users.noreplay.github.com>
2023-02-08 11:03:02 +0100

    Added best of 5 rounds

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
-let userChoice = prompt("Choose one: ROCK, PAPER OR SCISSORS!!!");
-console.log(userChoice)
+let i = 0;
+let k = 0;
 
-let compGuess = ["ROCK", "PAPER", "SCISSORS"];
-let randomChoice = Math.floor(Math.random()*compGuess.length);
-console.log (compGuess[randomChoice]);
+console.log("USER:  " + i);
+console.log("COMPUTER:  " + k);
 
-
-if ((userChoice) == (compGuess[randomChoice])) {
-    alert("IT'S A DRAW!");
-
-}else if (userChoice == compGuess[0] && compGuess[randomChoice] == compGuess[2]){
-    alert("YOU WON!");
-
-}else if (userChoice == compGuess[1] && compGuess[randomChoice] == compGuess[0]){
-    alert("YOU WON!");
-
-}else if (userChoice == compGuess[2] && compGuess[randomChoice] == compGuess[1]){
-    alert("YOU WON!");
-
-} else {
-    alert("YOU LOST!");
+if (i == 3){
+    alert("USER WON!")
+}else if (k == 3){
+    alert(" COMPUTER WON!")
+}else {alert("NEW ROUND!")
 }
+    while (i<3, k<3){
+
+    let userChoice = prompt("Choose one: ROCK, PAPER OR SCISSORS!!!");{
+    console.log(userChoice);
+    }
+    let compGuess = ["ROCK", "PAPER", "SCISSORS"];
+    let randomChoice = Math.floor(Math.random()*compGuess.length);
+    console.log (compGuess[randomChoice]);
+    
+    if ((userChoice) == (randomChoice)) {
+            alert("IT'S A DRAW!");
+
+
+    }else if (userChoice == compGuess[0] && compGuess[randomChoice] == compGuess[2]){
+            i++;
+            alert("YOU WON!");
+        
+
+    }else if (userChoice == compGuess[1] && compGuess[randomChoice] == compGuess[0]){
+             i++;
+            alert("YOU WON!")
+
+    }else if (userChoice == compGuess[2] && compGuess[randomChoice] == compGuess[1]){
+            i++;
+            alert("YOU WON!");
+    
+
+    }else {
+            k++;
+            alert("YOU LOST!");
+    
+    }
+    console.log("USER:  " + i);
+    console.log("COMPUTER:  " + k);
+}
+
+
 
